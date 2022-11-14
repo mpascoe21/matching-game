119e144e45452f942e7ef5fc2d087aaffb783299
maria.pascoe <maria.pascoe@wearetwogether.com>
2022-11-14 11:43:35 +0000

    Created Intro component component.

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,14 +1,36 @@
 import React from "react";
-import './styles.module.scss';
+import styles from './styles.module.scss';
+import IntroStaffCard from "../IntroStaffCard";
+import Tile from "../Tile";
+import LevelError from "../LevelError";
+import CountdownTimer from "../CountdownTimer";
+
 
 const Intro = () => {
+
+  const startGame = () => {
+
+  }
+
+
   return (
-    <div>
-      <h1>Speed Match</h1>
-      <p>Can you match all the team pairs before the time runs out?</p>
-      <p>Simply click two identical card in succession to create a match.<br>The fastest player to complete all three levels wins a spin.</p>
-      <h3>Welcome</h3>
-      <button>LET'S PLAY</button>
+    <div className={styles.background}>
+      <div className={styles.copyContainer}>
+        <h1>Speed Match</h1>
+        <p>Can you match all the team pairs before the time runs out?</p>
+        <p>Simply click two identical card in succession to create a match.<br/>The fastest player to complete all three levels wins a spin.</p>
+        <h4>Welcome</h4>
+        <a className={styles.button} onClick={() => startGame()}>LET'S PLAY</a>
+      </div>
+      <div className={styles.imgContainer}>
+        < IntroStaffCard />
+        < IntroStaffCard />
+        < IntroStaffCard />
+        < IntroStaffCard />
+        < IntroStaffCard />
+        < IntroStaffCard />
+      </div>
+
 
     </div>
 
