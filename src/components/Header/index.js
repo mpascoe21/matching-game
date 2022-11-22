f5c7fcd9e8816d478313f6ed2e19498d7103e1fa
maria.pascoe <maria.pascoe@wearetwogether.com>
2022-11-22 19:42:57 +0000

    Created header title component.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import styles from './styles.module.scss';
+import CountdownTimer from "../CountdownTimer";
+import HeaderTitle from "../HeaderTitle";
 
-const Header = () => {
+const Header = ({currentPage, timeLeft, setTimeLeft, countdown, currentLevel, teamName}) => {
+
+
+
+
   return (
-    <div className={styles.header}>
+    <div className={styles.header + ' ' + (currentPage === 'cardList' ? styles.gameHeader : '')}>
       <img src='/images/rhino-logo.png' alt="Twogether Rhino Logo"/>
-      <p>X Team / Level & Timer</p>
-      <div></div>
+      <div>
+        <HeaderTitle currentPage={currentPage}
+                     timeLeft={timeLeft}
+                     setTimeLeft={setTimeLeft}
+                     countdown={countdown}
+                     currentLevel={currentLevel}
+                     teamName={teamName}/>
+      </div>
+      <div className={styles.placeholder}></div>
     </div>
   );
 }
---
+++ src/components/HeaderTitle/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import CountdownTimer from "../CountdownTimer";
+import styles from './styles.module.scss';
+
+const HeaderTitle = ({currentPage, timeLeft, setTimeLeft, countdown, currentLevel, teamName}) => {
+
+  const introHeader = () => {
+    return <p className={styles.introTitle}>{teamName}</p>;
+  }
+
+  const cardListHeader = () => {
+    return <>
+      <p className={styles.cardListTitle}>Level {currentLevel}</p>
+      {/*<CountdownTimer*/}
+      {/*  timeLeft={timeLeft}*/}
+      {/*  setTimeLeft={setTimeLeft}*/}
+      {/*  countdown={countdown}/>*/}
+    </>;
+  }
+
+    return (
+        <div className={styles.titleContainer}>
+          {currentPage === 'cardList' ? cardListHeader() : introHeader()}
+          {/*<CountdownTimer*/}
+          {/*  timeLeft={timeLeft}*/}
+          {/*  setTimeLeft={setTimeLeft}*/}
+          {/*  countdown={countdown}/>*/}
+        </div>
+    );
+}
+
+export default HeaderTitle;
