dc5a7c3372170cb45d47a0f44e271ee38955f9d3
maria.pascoe <maria.pascoe@wearetwogether.com>
2022-11-24 16:19:55 +0000

    Pushing up latest changes.

--- src/components/CountdownTimer/index.js
+++ src/components/CountdownTimer/index.js
@@ -1,24 +1,30 @@
 import React, {useEffect, useState, useRef} from "react";
 import styles from './styles.module.scss';
 
-const CountdownTimer = () => {
+const CountdownTimer = ({timeLeft, setTimeLeft, countdown}) => {
 
-    const hasLoaded = useRef(false);
-    const [timeLeft, setTimeLeft] = useState(10);
+     const hasLoaded = useRef(false);
+    // const [timeLeft, setTimeLeft] = useState(10);
 
     useEffect(() => {
       if (hasLoaded.current) return;
       hasLoaded.current = true;
 
-      const countdown = () => {
-        setTimeout(() => {
-          setTimeLeft((timeLeft) => timeLeft - 1);
-
-          countdown();
-        }, 1000);
-      };
-
       countdown();
+      //
+      // const countdown = () => {
+      //   setTimeout(() => {
+      //       setTimeLeft((timeLeft) => timeLeft > 0 ? timeLeft - 1 : timeLeft);
+      //     countdown();
+      //   }, 1000);
+      // };
+
+      // return () => {
+      //   clearTimeout(countdown);
+      // }
+      //
+      // countdown();
+
     },[]);
 
   return (
